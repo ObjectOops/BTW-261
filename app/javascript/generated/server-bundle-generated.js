e7aa4da3c35c3f5bfbf9b8e5cbe3872d72b4f631
Alex Yao <92550885+ObjectOops@users.noreply.github.com>
2026-04-02 23:59:52 +0000

    Commit server bundle entrypoint.

---
+++ app/javascript/generated/server-bundle-generated.js
@@ -0,0 +1,7 @@
+import ReactOnRails from 'react-on-rails';
+
+import SimpleCounter from '../src/SimpleCounter/ror_components/SimpleCounter.tsx';
+import HelloWorld from '../src/HelloWorld/ror_components/HelloWorld.server.tsx';
+
+ReactOnRails.register({SimpleCounter,
+HelloWorld});
