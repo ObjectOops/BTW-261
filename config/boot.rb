1aedbd7e608ad3c33d77369a21f07ef4fa61c74d
Alex Yao <92550885+ObjectOops@users.noreply.github.com>
2026-04-03 00:04:45 +0000

    Use venv node.

--- config/boot.rb
+++ config/boot.rb
@@ -1,3 +1,5 @@
+ENV["PATH"] = File.expand_path("~/nodevenv/BTW-261/18/bin") + ":" + ENV["PATH"]
+
 ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../Gemfile", __dir__)
 
 require "bundler/setup" # Set up gems listed in the Gemfile.
